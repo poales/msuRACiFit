bb6bab550dc3da11227f874f69cdcde60d2fd058
poales <amcclain02@gmail.com>
2021-02-25 13:24:31 -0500

    looks like i forgot to make maxiter work

--- R/fitAci.R
+++ R/fitAci.R
@@ -38,7 +38,7 @@ fitACi <- function(data,name_assimilation ="A",name_ci=c("pCi","Ci"),gammastar=3
   bound_l <- bound_l[is.na(forceValues)]
   bound_h <- bound_h[is.na(forceValues)]
   print(maxiter)
-  myfit <- minpack.lm::nls.lm(par=initialGuess,lower=bound_l,upper = bound_h,fn = myFun,control = minpack.lm::nls.lm.control(maxiter=250,maxfev = 1250))
+  myfit <- minpack.lm::nls.lm(par=initialGuess,lower=bound_l,upper = bound_h,fn = myFun,control = minpack.lm::nls.lm.control(maxiter=maxiter,maxfev = 1250))
   print(myfit)
   return(myfit)
 }
