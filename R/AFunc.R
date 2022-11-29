06901479cbd95aad6b6a8996de0964d2562321b7
Alan McClain <amcclain02@gmail.com>
2022-11-29 12:47:07 -0500

    Revert "sped up AFunc"
    This reverts commit 90e3163b055dd84300454d9d64f2be5db2361730.

--- R/AFunc.R
+++ R/AFunc.R
@@ -28,7 +28,12 @@ AFunc <- function(Cc, aG, aS, Rd, Vcmax, j, TPU, gm,Kc,Ko,O2,gammastar){
   ac = AcFunc(Cc, Rd, Vcmax, Kc,Ko,O2, coef)
   aj = AjFunc(Cc, aG, aS, Rd, j, gammastar, coef)
   ap = ApFunc(Cc, aG, aS, Rd, TPU, gammastar, coef)
-  #pick which one is closest to Rd, which should be the pivot point when you're below 0
-  pmin(abs(ac+Rd),abs(aj+Rd),abs(ap+Rd))
+  tot <- tibble::tibble(ac,aj,ap,.name_repair = "minimal") #put them in a table to help pick which one is smallest
+  l <- apply(tot,1,function(x)which.min(abs(x+Rd))) #pick which one is closest to Rd - this is the pivot point when you go below 0, not absolute 0
+  res <- 1:length(l) #create output vector
+  for(i in 1:length(l)){
+    res[i] <- tot[[i,l[[i]]]] #input the selected lowest value into the output vector
+  }
+  return(res)
 }
 
